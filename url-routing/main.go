d22ff9d511c0eb86612089c3f1b50055d63bba6e
Aleksey <alekssum@gmail.com>
2020-03-25 09:18:57 +0300

    file server

--- url-routing/main.go
+++ url-routing/main.go
@@ -18,7 +18,8 @@ func main() {
     // Posts singular
     r.GET("/posts/:id", PostShowHandler)
     r.PUT("/posts/:id", PostUpdateHandler)
-    r.GET("/posts/:id/edit", PostEditHandler)
+	r.GET("/posts/:id/edit", PostEditHandler)
+	r.ServeFiles("/src/*filepath", http.Dir("src"))
 
     fmt.Println("Starting server on :8080")
     http.ListenAndServe(":8080", r)
