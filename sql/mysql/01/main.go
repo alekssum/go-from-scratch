2274798c4a0fd2a2be0ea6b0b758ae2315f412c1
alekssum <alekssum@gmail.com>
2020-04-16 19:50:21 +0300

    simple sql query

--- sql/mysql/01/main.go
+++ sql/mysql/01/main.go
@@ -2,19 +2,49 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "root:@/employees")
+type handler struct {
+	DB *sql.DB
+}
+
+var H handler
+
+func init() {
+	var err error
+	H.DB, err = sql.Open("mysql", "root:@/employees")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
-	if err = db.Ping(); err != nil {
+	if err = H.DB.Ping(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type department struct {
+	deptNo, deptName string
+}
+
+func main() {
+
+	defer H.DB.Close()
+
+	rows, err := H.DB.Query("SELECT * FROM departments LIMIT 10")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	result := []department{}
+	for rows.Next() {
+		var d department
+		rows.Scan(&d.deptNo, &d.deptName)
+		result = append(result, d)
+	}
+
+	fmt.Println(result)
 }
