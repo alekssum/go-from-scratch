9bdb5d2954c1211697974a116fd60b3adc1487b4
alekssum <alekssum@gmail.com>
2020-04-16 07:22:08 +0300

    added connection to mysql

---
+++ sql/mysql/01/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@/employees")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
